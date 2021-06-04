551bb762335c7ace9f17169ddf5e5fb07dd2fea9
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2021-06-04 15:09:20 -0300

    Added 'get_data.py' script to retrieve and process the dataset

---
+++ src/get_data.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config):
+    data_path = config["data_source"]["gdrive"]     
+    df = pd.read_csv(data_path)
+    return df
+
+if __name__ == "__main__":
+
+## Step 1: Read the parameters
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    
+    config = read_params(parsed_args.config)
+## Step 2: Process the data
+    data = get_data(config)
+    print(data.head())
+## Step 3: Return a new dataframe containing the data
