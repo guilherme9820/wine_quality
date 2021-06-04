153ea176974a8f5907db01dbd663cfe423d0771f
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2021-06-04 17:50:43 -0300

    Added training and evaluation routines; Fixed 'target_col' value in params.yaml

---
+++ src/train_and_evaluation.py
@@ -0,0 +1,63 @@
+# Load the train and test
+# train the model
+# Save the metrics, parameters
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from get_data import read_params
+import argparse
+import joblib
+import json
+
+def eval_metrics(groundtruth, predictions):
+
+    rmse = np.sqrt(mean_squared_error(groundtruth, predictions))
+    mae = mean_absolute_error(groundtruth, predictions)
+    r2 = r2_score(groundtruth, predictions)
+
+    return rmse, mae, r2
+
+def train_and_evaluate(config):
+    
+    train_data_path = config["split_data"]["train_path"]
+    test_data_path = config["split_data"]["test_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dir"]
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+
+    target = [config["base"]["target_col"]]
+
+    train = pd.read_csv(train_data_path)
+    test = pd.read_csv(test_data_path)
+    
+    train_y = train[target]
+    test_y = test[target]
+
+    train_x = train.drop(target, axis=1)
+    test_x = test.drop(target, axis=1)
+
+    model = ElasticNet(alpha=alpha,
+                       l1_ratio=l1_ratio,
+                       random_state=random_state)
+    model.fit(train_x, train_y)
+    
+    predicted_qualities = model.predict(test_x)
+    
+    rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
+    
+    print(rmse, mae, r2)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    
+    config = read_params(parsed_args.config)
+
+    train_and_evaluate(config)
