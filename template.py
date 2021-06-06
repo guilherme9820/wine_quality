5b4f1e3b205cfd1d273ccb023bce6734864c5b17
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2021-06-06 10:30:47 -0300

    Updated the project structure; Added a website layout for the project

--- template.py
+++ template.py
@@ -9,9 +9,9 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 dirs = [
     join(["data", "raw"]),
     join(["data", "processed"]),
+    join(["prediction_service", "model"]),
     "notebooks",
-    "saved_models",
-    "src"]
+    "saved_models"]
 
 for dir_ in dirs:
     filedir = join([root_path, dir_, ".gitkeep"])
@@ -22,12 +22,20 @@ files = [
     "dvc.yaml",
     "params.yaml",
     ".gitignore",
+    "app.py",
+    join(["prediction_service", "__init__.py"]),
+    join(["prediction_service", "prediction.py"]),
     join(["src", "__init__.py"]),
     join(["reports", "params.json"]),
     join(["reports", "scores.json"]),
     join(["tests", "conftest.py"]),
     join(["tests", "test_config.py"]),
     join(["tests", "__init__.py"]),
+    join(["webapp", "static", "css", "main.css"]),
+    join(["webapp", "templates", "index.html"]),
+    join(["webapp", "templates", "404.html"]),
+    join(["webapp", "templates", "base.html"]),
+    join(["webapp", "static", "script", "index.js"]),
     "README.md"]
 
 for file_ in files:
